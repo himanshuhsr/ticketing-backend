2656c76c4274731a76a26f920e82255960f2f3eb
HSR Hi-Tech Solutions <rajh91774@gmail.com>
2022-08-07 13:20:20 +0530

    Applied Morgan
    Added scripts to log data to access.log file inside the src folder.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/config');
+const morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
 
 // Database connection
 mongoose
@@ -11,7 +14,17 @@ mongoose
         console.log(err);
     });
 
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use(express.json());
+
+app.get('/', function (req, res) {
+    res.send('hello, world!');
+});
 
 app.listen(config.server.port, () => {
     console.log('Backend Server is running!');
