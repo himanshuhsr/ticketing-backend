06b183f51e24d26977e8d3afe7d6075569ddb175
HSR Hi-Tech Solutions <rajh91774@gmail.com>
2022-08-07 13:15:17 +0530

    Database Connection
    Completed scripts for connection to the database

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,14 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 2005;
+
+module.exports = {
+    mongo: {
+        url: MONGO_URL
+    },
+    server: {
+        port: SERVER_PORT
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/config');
+
+// Database connection
+mongoose
+    .connect(config.mongo.url)
+    .then(() => console.log('DB Connection Successful!'))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(express.json());
+
+app.listen(config.server.port, () => {
+    console.log('Backend Server is running!');
+});
